1e0dcefbd6d5c304f1a19d26d11598cba8bf3c3d
Isar Ramses <rammses_@live.com.mx>
2020-01-10 03:02:05 -0600

    create dummy a component

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,16 +1,18 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation';
-import HomeScreen from './src/screens/HomeScreen';
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+import HomeScreen from './src/screens/HomeScreen'
+import ComponentScreen from './src/screens/ComponentsScreen'
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Home: HomeScreen,
+    Component: ComponentScreen
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Component',
     defaultNavigationOptions: {
       title: 'App'
     }
   }
-);
+)
 
-export default createAppContainer(navigator);
+export default createAppContainer(navigator)
